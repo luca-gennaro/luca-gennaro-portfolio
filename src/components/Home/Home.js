046b06c2b8b6f566a39aa275eae2524b6e1eb55f
Luca Gennaro <lucagennaro91@gmail.com>
2023-02-18 22:26:55 +0100

    fix heroImage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,52 +2,54 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import logoL from "../../assets/images/logo-l.png"
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
-import Logo from './Logo/Logo'
+import heroImage from "../../assets/images/heroImage.png"
 import "./Home.scss"
 import Loader from 'react-loaders'
 
 const Home = () => {
-    const [letterClass, setLetterClass] = useState("text-animate")
-    const nameArray = ["u", "c", "a", " ", "G", "e", "n", "n", "a", "r", "o"]
-    const jobArray = ["W", "e" ,"b" , " ", "D", "e", "v", "e", "l", "o", "p", "e", "r"]
+  const [letterClass, setLetterClass] = useState("text-animate")
+  const nameArray = ["u", "c", "a", " ", "G", "e", "n", "n", "a", "r", "o"]
+  const jobArray = ["W", "e", "b", " ", "D", "e", "v", "e", "l", "o", "p", "e", "r", "."]
 
-    useEffect(() => {
-        const timerId = setTimeout(() => {
-          setLetterClass("text-animate-hover")
-        }, 4000);
-      
-        return () => {
-          clearTimeout(timerId);
-        };
-      }, []);
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setLetterClass("text-animate-hover")
+    }, 4000);
 
-    return (
-        <>
-          <div className='container home-page'>
-              <div className='text-zone'>
-                  <h1>
-                      <span className={letterClass}>C</span>
-                      <span className={`${letterClass} _8`}>i</span>
-                      <span className={`${letterClass} _9`}>a</span>
-                      <span className={`${letterClass} _10`}>o,</span>
-                      <br /> 
-                      <span className={`${letterClass} _11`}>s</span>
-                      <span className={`${letterClass} _12`}>o</span>
-                      <span className={`${letterClass} _13`}>n</span>
-                      <span className={`${letterClass} _14`}>o</span>
-                  <img src={logoL} alt="developer" />
-                  <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
-                  <br />
-                  <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={26} />
-                  </h1>
-                  <h2>Full Stack Developer</h2>
-                  <Link to="/contact" className='flat-button'>CONTATTAMI</Link>
-              </div>
-              <Logo />
-          </div>
-          <Loader type='pacman'/>
-        </>
-    )
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, []);
+
+  return (
+    <>
+      <div className='container home-page'>
+        <div className='text-zone'>
+          <h1>
+            <span className={letterClass}>C</span>
+            <span className={`${letterClass} _8`}>i</span>
+            <span className={`${letterClass} _9`}>a</span>
+            <span className={`${letterClass} _10`}>o,</span>
+            <br />
+            <span className={`${letterClass} _11`}>s</span>
+            <span className={`${letterClass} _12`}>o</span>
+            <span className={`${letterClass} _13`}>n</span>
+            <span className={`${letterClass} _14`}>o</span>
+            <img src={logoL} alt="developer" />
+            <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
+            <br />
+            <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={26} />
+          </h1>
+          <h2>Full Stack Developer</h2>
+          <Link to="/contact" className='flat-button'>CONTATTAMI</Link>
+        </div>
+        <div className='logo-container'>
+          <img className='solid-logo' src={heroImage} alt="L" />
+        </div>
+      </div>
+      <Loader type='pacman' />
+    </>
+  )
 }
 
 export default Home
