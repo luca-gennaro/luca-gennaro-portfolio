30bf0bb0b12be1cab1598fc417ceffb7138a53c8
Luca Gennaro <lucagennaro91@gmail.com>
2023-02-18 21:47:34 +0100

    loader pacman

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,6 +4,7 @@ import logoL from "../../assets/images/logo-l.png"
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
 import Logo from './Logo/Logo'
 import "./Home.scss"
+import Loader from 'react-loaders'
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState("text-animate")
@@ -21,28 +22,31 @@ const Home = () => {
       }, []);
 
     return (
-        <div className='container home-page'>
-            <div className='text-zone'>
-                <h1>
-                    <span className={letterClass}>C</span>
-                    <span className={`${letterClass} _8`}>i</span>
-                    <span className={`${letterClass} _9`}>a</span>
-                    <span className={`${letterClass} _10`}>o,</span>
-                    <br /> 
-                    <span className={`${letterClass} _11`}>s</span>
-                    <span className={`${letterClass} _12`}>o</span>
-                    <span className={`${letterClass} _13`}>n</span>
-                    <span className={`${letterClass} _14`}>o</span>
-                <img src={logoL} alt="developer" />
-                <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
-                <br />
-                <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={26} />
-                </h1>
-                <h2>Full Stack Developer</h2>
-                <Link to="/contact" className='flat-button'>CONTATTAMI</Link>
-            </div>
-            <Logo />
-        </div>
+        <>
+          <div className='container home-page'>
+              <div className='text-zone'>
+                  <h1>
+                      <span className={letterClass}>C</span>
+                      <span className={`${letterClass} _8`}>i</span>
+                      <span className={`${letterClass} _9`}>a</span>
+                      <span className={`${letterClass} _10`}>o,</span>
+                      <br /> 
+                      <span className={`${letterClass} _11`}>s</span>
+                      <span className={`${letterClass} _12`}>o</span>
+                      <span className={`${letterClass} _13`}>n</span>
+                      <span className={`${letterClass} _14`}>o</span>
+                  <img src={logoL} alt="developer" />
+                  <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
+                  <br />
+                  <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={26} />
+                  </h1>
+                  <h2>Full Stack Developer</h2>
+                  <Link to="/contact" className='flat-button'>CONTATTAMI</Link>
+              </div>
+              <Logo />
+          </div>
+          <Loader type='pacman'/>
+        </>
     )
 }
 
