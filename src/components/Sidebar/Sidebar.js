5a4a0b07a65d79b7ad6b834e28c6f67474608c1b
Luca Gennaro <lucagennaro91@gmail.com>
2023-02-21 14:07:03 +0100

    another fix nav

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -13,16 +13,16 @@ const Sidebar = () => {
     return (
         <div className='nav-bar'>
             <nav className={showNav ? "mobile-show" : ""}>
-                <NavLink exact="true" activeclassname="active" to="/">
+                <NavLink exact="true" activeclassname="active" to="/" onClick={()=> {setShowNav(false)}}>
                     <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
                 </NavLink>
-                <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <NavLink exact="true" activeclassname="active" className="about-link" to="/about" onClick={()=> {setShowNav(false)}}>
                     <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
                 </NavLink>
-                <NavLink exact="true" activeclassname="active" className="portfolio-link" to="/portfolio">
+                <NavLink exact="true" activeclassname="active" className="portfolio-link" to="/portfolio" onClick={()=> {setShowNav(false)}}>
                     <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
                 </NavLink>
-                <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact" onClick={()=> {setShowNav(false)}}>
                     <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
                 </NavLink>
                 <FontAwesomeIcon
