86adc641dae4463e735bf45e898acebfa8d7a73d
Luca Gennaro <lucagennaro91@gmail.com>
2023-02-21 22:06:30 +0100

    fix mq about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -30,8 +30,10 @@ const About = () => {
                             idx={15}
                         />
                     </h1>
-                    <p>Mi chiamo Luca Gennaro, ho 32 anni. Appassionato di informatica e tecnologia. Inizio da piccolo a "smanettare" e approfondire gli argomenti con delle riviste informatiche e assemblando i miei primi Pc, così da approfondire anche l'aspetto hardware. Conseguo il diploma di Perito Elettronico e Telecomunicazioni. Sperimento software e tecnologie anche nell'ambito della musica, altra passione che mi accompagna da sempre.</p>
-                    <p>Una cosa che mi ha da sempre incuriosito e non ho mai approfondito a dovere è la programmazione web, così mi sono messo in gioco frequentando un corso intensivo per diventare Full-Stack Developer e qui ho scoperto un mondo spettacolare e stimolante. Cerco di diventare ogni giorno la versione migliore di me stesso in ogni ambito, mi piace sfidarmi e superare i miei limiti. Il mio obbiettivo è poter dare il mio contributo come programmatore nel realizzare e sviluppare progetti significativi che possano impattare positivamente sulla vita delle persone.</p>
+                    <div className="about-text">
+                        <p>Mi chiamo Luca Gennaro, ho 32 anni. Appassionato di informatica e tecnologia. Inizio da piccolo a "smanettare" e approfondire gli argomenti con delle riviste informatiche e assemblando i miei primi Pc, così da approfondire anche l'aspetto hardware. Conseguo il diploma di Perito Elettronico e Telecomunicazioni. Sperimento software e tecnologie anche nell'ambito della musica, altra passione che mi accompagna da sempre.</p>
+                        <p>Una cosa che mi ha da sempre incuriosito e non ho mai approfondito a dovere è la programmazione web, così mi sono messo in gioco frequentando un corso intensivo per diventare Full-Stack Developer e qui ho scoperto un mondo spettacolare e stimolante. Cerco di diventare ogni giorno la versione migliore di me stesso in ogni ambito, mi piace sfidarmi e superare i miei limiti. Il mio obbiettivo è poter dare il mio contributo come programmatore nel realizzare e sviluppare progetti significativi che possano impattare positivamente sulla vita delle persone.</p>
+                    </div>
                 </div>
 
                 <div className='logo-container'>
